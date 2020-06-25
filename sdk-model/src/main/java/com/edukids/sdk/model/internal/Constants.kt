ee2ad5a69a2a39fcae6c0cd40d3e0d94126689e3
Viktor De Pasquale <info@depasquale.wiki>
2020-06-25 14:44:00 +0200

    Updated the app to be launchable only as Edu app

--- sdk-model/src/main/java/com/edukids/sdk/model/internal/Constants.kt
+++ sdk-model/src/main/java/com/edukids/sdk/model/internal/Constants.kt
@@ -5,6 +5,8 @@ object Constants {
     const val CLASS_LAUNCHER_RECEIVER = "com.edukids.comms.SdkBroadcastReceiver"
     const val PACKAGE_LAUNCHER = "com.edukids.launcher"
 
+    const val CATEGORY_EDU_LAUNCHER = "com.edukids.LAUNCHER"
+
     const val ACTION_CONTACT_DATA_SOURCE = "com.edukids.sdk.ACTION_CONTACT_DATA_SOURCE"
     const val ACTION_RECEIVE_DATA = "com.edukids.sdk.ACTION_RECEIVE_DATA"
 
