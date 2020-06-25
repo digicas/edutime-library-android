420bba2ebbc94f7a7cb664d09a81a97d015a60b5
Viktor De Pasquale <info@depasquale.wiki>
2020-06-25 14:42:06 +0200

    Added logging

--- app/src/main/java/com/edukids/sdk/app/App.kt
+++ app/src/main/java/com/edukids/sdk/app/App.kt
@@ -1,6 +1,8 @@
 package com.edukids.sdk.app
 
 import android.app.Application
+import android.util.Log
+import com.edukids.sdk.model.logging.SdkLogger
 import timber.log.Timber
 
 class App : Application() {
@@ -8,6 +10,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        SdkLogger.level(Log.VERBOSE)
     }
 
 }
