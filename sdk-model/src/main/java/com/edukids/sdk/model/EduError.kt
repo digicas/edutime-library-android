b5efd054b75402b6cd9064dab807b0db892565ad
Viktor De Pasquale <info@depasquale.wiki>
2020-07-03 17:17:59 +0200

    Added special error codes

--- sdk-model/src/main/java/com/edukids/sdk/model/EduError.kt
+++ sdk-model/src/main/java/com/edukids/sdk/model/EduError.kt
@@ -16,7 +16,33 @@ data class EduError(
 
     companion object {
 
+        /**
+         * This error has unknown origin. Report use-case to the developer in order to get more
+         * information.
+         * */
         const val ERR_UNKNOWN = -1
+
+        /**
+         * No more coins can be supplied hence no additional mission can be started at this time.
+         * Check with [com.edukids.sdk.EduSdkInstance.getCurrencyStats] to prevent this error.
+         * */
+        const val ERR_COIN_BUDGET = 1
+
+        /**
+         * You cannot mock your own package name or instance ids.
+         * */
+        const val ERR_SECURITY = 2
+
+        /**
+         * Instance you've given to the sdk is invalid and does not exist. By violating this rule
+         * multiple times you'll be revoked the communication with the sdk.
+         * */
+        const val ERR_INVALID_INSTANCE = 3
+
+        /**
+         * Mission contract does not exist or is invalid.
+         * */
+        const val ERR_INVALID_CONTRACT = 4
 
     }
 
